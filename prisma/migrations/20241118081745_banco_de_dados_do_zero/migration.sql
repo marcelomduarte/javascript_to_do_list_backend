efbdc542a8606a0d655c7dbb36d8ef1305feff38
Marcelo MD <marcelomduarte30@outlook.com>
2024-11-18 05:57:11 -0300

    trocado provider de mysql para mongodb

--- prisma/migrations/20241118081745_banco_de_dados_do_zero/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `Tarefa` (
-    `id` VARCHAR(191) NOT NULL,
-    `descricao` VARCHAR(191) NOT NULL,
-    `completa` BOOLEAN NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
